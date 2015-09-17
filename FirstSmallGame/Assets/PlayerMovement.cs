1a7a89580cabe6bda64a23880bfa467c2cc0aab3
Dakota Polenz <polenzdakota@yahoo.com>
2015-09-16 22:16:39 -0700

    Started first game

---
+++ FirstSmallGame/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public GameObject bullet;
+	public float speed = 6;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movement = Input.GetAxis("Horizontal");
+		transform.Translate (speed * movement, 0, 0);
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Instantiate (bullet, transform.position, transform.rotation);
+		}
+
+	}
+}
---
+++ FirstSmallGame/Assets/bulletMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletMovement : MonoBehaviour {
+	public float bulletSpeed = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, bulletSpeed, 0);
+		if (transform.position.y > 50) {
+			DestroyObject (gameObject);
+		}
+	}
+}
