69386c4018314ca79f12d3cfdffb91d73ecb84cc
Dakota Polenz <polenzdakota@yahoo.com>
2015-09-30 20:30:22 -0700

    Made basic game concept

--- FirstSmallGame/Assets/PlayerMovement.cs
+++ FirstSmallGame/Assets/PlayerMovement.cs
@@ -4,20 +4,31 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	public GameObject bullet;
 	public float speed = 6;
+	private int collisionCount;
 
 	// Use this for initialization
 	void Start () {
-	
+		collisionCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float movement = Input.GetAxis("Horizontal");
-		transform.Translate (speed * movement, 0, 0);
+		float movement_X = Input.GetAxis("Horizontal");
+		float movement_Y = Input.GetAxis ("Vertical");
+		transform.Translate (speed * movement_X, 0, 0);
+		transform.Translate (0, speed * movement_Y, 0);
 
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			Instantiate (bullet, transform.position, transform.rotation);
 		}
+	}
 
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.name != "bullet(Clone)") {
+			print ("Player Collided with: " + col.gameObject.name);
+			collisionCount++;
+			print ("Number of collisions with player: " + collisionCount);
+			Destroy (col.gameObject);
+		}
 	}
 }
--- FirstSmallGame/Assets/bulletMovement.cs
+++ FirstSmallGame/Assets/bulletMovement.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class bulletMovement : MonoBehaviour {
 	public float bulletSpeed = 5;
+	private int collisionCount;
 	// Use this for initialization
 	void Start () {
-	
+		collisionCount = 0;
 	}
 	
 	// Update is called once per frame
@@ -13,6 +14,15 @@ public class bulletMovement : MonoBehaviour {
 		transform.Translate (0, bulletSpeed, 0);
 		if (transform.position.y > 50) {
 			DestroyObject (gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.name != "Player") {
+			print ("Bullet Collided with: " + col.gameObject.name);
+			collisionCount++;
+			print ("Number of collisions with Bullet: " + collisionCount);
+			Destroy (col.gameObject);
 		}
 	}
 }
