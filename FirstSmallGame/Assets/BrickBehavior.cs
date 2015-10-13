9603c63c4c8eb7eecdd2ea30221bdca09da847d6
Dakota Polenz <polenzdakota@yahoo.com>
2015-10-13 12:53:48 -0700

    Finished brick scripts
    Still a little weird

--- FirstSmallGame/Assets/BrickBehavior.cs
+++ FirstSmallGame/Assets/BrickBehavior.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class BrickBehavior : MonoBehaviour, EnemKillable {
-
 	public int brickHP = 3;
+	public float brickSpeed = 2;
+	public int range = 10;
+	public float initialX;
 	// Use this for initialization
 	void Start () {
+		initialX = transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		MoveBackAndForth (brickSpeed);
 	}
 
 	/// <summary>
@@ -20,6 +24,17 @@ public class BrickBehavior : MonoBehaviour, EnemKillable {
 		print (brickHP);
 		if (brickHP < 1) {
 			Destroy (gameObject);
+		}
+	}
+
+	public void MoveBackAndForth(float speed) {
+		transform.Translate (speed, 0, 0);
+		float position = transform.position.x;
+		float maxRangePos = range + initialX;
+		float maxRangeNeg = -range + initialX; 
+
+		if((position > 0 && position > maxRangePos) || (position < 0 && position < maxRangeNeg)) {
+			brickSpeed = -brickSpeed;
 		}
 	}
 }
