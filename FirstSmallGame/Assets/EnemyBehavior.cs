a4b2be03e88e908857e3308806d54b331a346112
Dakota Polenz <polenzdakota@yahoo.com>
2015-10-14 14:12:30 -0700

    Added basic screen layout and enemy behavior
    Enemy does not yet attack

---
+++ FirstSmallGame/Assets/EnemyBehavior.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour, EnemKillable {
+	public GameObject EnemBullet;
+	public int enemHP = 15;
+	public int range = 10;
+	public float enemSpeed = 0.5f;
+	private float initialX;
+	public float frequency = 1f;
+	private float timedFire;
+	private float startTime;
+	private float prevTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		initialX = transform.position.x;
+		startTime = Time.time;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timedFire = Time.time - startTime - prevTime;
+		print (timedFire);
+		if (timedFire > frequency) {
+			FireBullet();
+			prevTime = Time.time;
+			timedFire = 0;
+		}
+		MoveBackAndForth (enemSpeed);
+	}
+
+	public void OnHit() {
+		enemHP--;
+		print (enemHP);
+		if (enemHP < 1) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void FireBullet() {
+		print ("fire");
+		//Instantiate (EnemBullet, transform.position, transform.rotation);
+	}
+
+	public void MoveBackAndForth(float speed) {
+		transform.Translate (speed, 0, 0);
+		float position = transform.position.x;
+		float maxRangePos = range + initialX;
+		float maxRangeNeg = -range + initialX; 
+		
+		if((position > 0 && position > maxRangePos) || (position < 0 && position < maxRangeNeg)) {
+			enemSpeed = -enemSpeed;
+		}
+	}
+}
--- FirstSmallGame/Assets/PlayerMovement.cs
+++ FirstSmallGame/Assets/PlayerMovement.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	public GameObject bullet;
-	public float speed = 6;
+	public float speed = 6f;
 	public int playerHP = 10;
+	public float range = 10f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,11 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float movement_X = Input.GetAxis("Horizontal");
-		transform.Translate (speed * movement_X, 0, 0);
+
+
+		if (bounds (movement_X)) {
+			transform.Translate (speed * movement_X, 0, 0);
+		}
 
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			Instantiate (bullet, transform.position, transform.rotation);
@@ -24,11 +29,15 @@ public class PlayerMovement : MonoBehaviour {
 		}
 	}
 
+	bool bounds(float movement_X) {
+		return ((transform.position.x <= range && transform.position.x >= -range) ||
+			(transform.position.x > range && movement_X < 0) ||
+			(transform.position.x < -range && movement_X > 0));
+	}
+
 	void OnTriggerEnter(Collider col) {
 		if (col.gameObject.name != "bullet(Clone)") {
-			print ("Player Collided with: " + col.gameObject.name);
 			playerHP--;
-			print ("Number of collisions with player: " + playerHP);
 		}
 	}
 }
